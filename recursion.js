3147b99c452eb111fd0f1c107696688931b1cda5
Hunter Kreshock <hkreshock@gmail.com>
2019-04-01 09:19:59 -0700

    drill 3

--- recursion.js
+++ recursion.js
@@ -1,4 +1,4 @@
-
+'use strict';
 // function countSheep(num){
 //     if(num === 0) {
 //         return console.log('All sheep jumped over the fence')
@@ -9,16 +9,34 @@
 
 // countSheep(3);
 
-function powerCalculator(num, power){
-    if(power < 0){
-        return console.log('exponent should be >= 0')
-    }
+// function powerCalculator(num, power){
+//     if(power < 0){
+//         return console.log('exponent should be >= 0')
+//     }
 
-    if (power === 0){
-        return 1;
-    }
+//     if (power === 0){
+//         return 1;
+//     }
 
-    return num * powerCalculator(num, power-1);
+//     return num * powerCalculator(num, power-1);
+// }
+
+// console.log(powerCalculator(10, 2));
+
+// function reverseString(str, index = 0) {
+//   if (index === str.length - 1) {
+//     return str.charAt(index);
+//   }
+//   return reverseString(str, index + 1) + str.charAt(index);
+// }
+
+// console.log(reverseString('hunter'));
+
+function makeTriangle(n) {
+  if (n === 0) {
+    return 0;
+  }
+  const node = 0;
+
+  return makeTriangle(n);
 }
-
-console.log(powerCalculator(10, 2));
